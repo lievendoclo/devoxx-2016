fc92459e3d3f0672a05bcd5771060d2a6a37648a
Lieven Doclo <lieven.doclo@aca-it.be>
2016-11-03 10:21:22 +0100

    use eureka again

--- service-two/src/main/java/be/insaneprogramming/devoxx/ServiceTwoEndpoint.java
+++ service-two/src/main/java/be/insaneprogramming/devoxx/ServiceTwoEndpoint.java
@@ -3,26 +3,44 @@ package be.insaneprogramming.devoxx;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 @EnableDiscoveryClient
-public class ServiceOneEndpoint {
+//@RibbonClient("service-two")
+public class ServiceTwoEndpoint {
+
+	@LoadBalanced
+	@Bean
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ServiceOneEndpoint.class).run(args);
+		new SpringApplicationBuilder(ServiceTwoEndpoint.class).run(args);
 	}
 
 	@RequestMapping("/hello")
 	@ResponseBody
-	public String helloServiceOne() {
-		return "Hello from service one (" + getIp() + ")";
+	public String helloServiceTwo() {
+		return "Hello from service two (" + getIp() + ") with a " + getMessageFromServiceOne();
+	}
+
+	private String getMessageFromServiceOne() {
+		return restTemplate.getForObject("http://service-one/hello", String.class);
 	}
 
 	private String getIp() {
